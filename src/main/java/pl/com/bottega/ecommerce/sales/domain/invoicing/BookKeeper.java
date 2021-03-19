4e0853e6abcb4740333e872110dc5302f34c3997
kamil <222574@edu.p.lodz.pl>
2021-03-19 17:28:03 +0100

    fixing problems in BookKeeper class

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
@@ -21,10 +21,9 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class BookKeeper {
 
-    public Invoice issuance(ClientData client, List<RequestItem> items) {
-        Invoice invoice = new Invoice(Id.generate(), client);
-
-        for (RequestItem item : items) {
+    public Invoice issuance(InvoiceRequest invoiceRequest) {
+        Invoice invoice = Invoice.invoiceCreator(Id.generate(), invoiceRequest.getClient());
+        for (RequestItem item : invoiceRequest.getItems()) {
             Money net = item.getTotalCost();
             BigDecimal ratio = null;
             String desc = null;
